49d3bf32dbba8913e2cde8c4ac321e71bc7ae89d
adrian <adrian-perez@msn.com>
2024-07-05 23:38:37 +0200

    3ra subida a produccion quito imagenes

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -75,36 +75,8 @@ export const initialData: SeedData = {
       title: "Tarifa Contrato Vodafone",
       gender: 'contrato'
     },
-    {
-      description: "Tarifa Contrato Vodafone",
-      images: [
-        'logovodafone.jpg',
-        'logovodafone.jpg',
-      ],
-      inStock: 100,
-      price: 20,
-      sizes: [ "L_20", ],
-      slug: "contrato_vodafone_20",
-      type: 'vodafone',
-      tags: [ 'jacket' ],
-      title: "Contrato Vodafone 20",
-      gender: 'contrato'
-    },
-    {
-      description: "Contrato Vodafonre",
-      images: [
-        'logovodafone.jpg',
-        'logovodafone.jpg',
-      ],
-      inStock: 100,
-      price: 40,
-      sizes: ["XL_40"],
-      slug: "contrato_vodafone",
-      type: 'vodafone',
-      tags: [ 'shirt' ],
-      title: "Contrato Vodafone 40",
-      gender: 'contrato'
-    },
+    
+    
     {
       description: "Tarifa Prepago S 100Gb",
       images: [
@@ -120,36 +92,8 @@ export const initialData: SeedData = {
       title: "Prepago Vodafone S",
       gender: 'prepago'
     },
-    {
-      description: "Prepago Vodafone M llamadas internacionales",
-      images: [
-        'logovodafone.jpg',
-        'logovodafone.jpg',
-      ],
-      inStock: 10,
-      price: 130,
-      sizes: [ "M_15" ],
-      slug: "women_chill_half_zip_cropped_hoodie",
-      type: 'orange',
-      tags: [ 'hoodie' ],
-      title: "Prepago vodafone M",
-      gender: 'prepago'
-    },
-    {
-      description: "Prepago L Vodafone 200GB.",
-      images: [
-        'logovodafone.jpg',
-        'logovodafone.jpg',
-      ],
-      inStock: 162,
-      price: 100,
-      sizes: [ "L_20" ],
-      slug: "prepago_vodafone_L",
-      type: 'vodafone',
-      tags: [ 'shirt' ],
-      title: "Women's Raven Joggers",
-      gender: 'prepago'
-    },
+    
+
     {
       description: "Fibra mas Tv Gratis.",
       images: [
@@ -158,42 +102,14 @@ export const initialData: SeedData = {
       ],
       inStock: 10,
       price: 30,
-      sizes: [ "S_10", "M_15", "L_20" ],
+      sizes: [ "S_10", ],
       slug: "Tv_Gratis",
       type: 'vodafone',
       tags: [ 'shirt' ],
       title: "Tv Gratis mas Fibra mas Tv",
       gender: 'fibra_tv'
     },
-    {
-      description: "Fibra mas Tv .",
-      images: [
-        'logovodafone.jpg',
-        'logovodafone.jpg',
-      ],
-      inStock: 10,
-      price: 65,
-      sizes: [ "S_10", "M_15", "L_20" ],
-      slug: "kids_cyberquad_bomber_jacket",
-      type: 'vodafone',
-      tags: [ 'shirt' ],
-      title: "v Gratis mas Fibra mas Tv",
-      gender: 'fibra_tv'
-    },
-    {
-      description: "Fibra mas TV .",
-      images: [
-        'logovodafone.jpg',
-        'logovodafone.jpg',
-      ],
-      inStock: 10,
-      price: 30,
-      sizes: [ "S_10", "M_15", "L_20" ],
-      slug: "kids_corp_jacket",
-      type: 'vodafone',
-      tags: [ 'shirt' ],
-      title: "v Gratis mas Fibra mas Tv",
-      gender: 'fibra_tv'
-    },
+    
+    
   ]
 };
